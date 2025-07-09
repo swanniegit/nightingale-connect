b8a3b3f6f689c8ed5edf87f4364739edce6e129f
swanniegit <49320416+swanniegit@users.noreply.github.com>
2025-07-09 20:14:04 +0200

    railway deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     react(),
+    visualizer({ open: true }),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
