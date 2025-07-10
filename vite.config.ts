af14b3d51aadb4d5a30e658122e14c3d4d8a3bf3
swanniegit <49320416+swanniegit@users.noreply.github.com>
2025-07-10 20:37:43 +0200

    try the slash v4

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
       "@": "client/src",
       "@shared": "shared",
       "@assets": "attached_assets",
-      "client/src": "/client/src",
     },
   },
   root: "client",
